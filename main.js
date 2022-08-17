e6c93ee635d8c090ee7af978ac1480351e659d2e
GameMaker <76964252+GameMaker38@users.noreply.github.com>
2022-08-17 17:27:27 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 let atoms = 0
-
-document.getElementById("atomcounter").innerHTML = "Atoms: " + atoms;
+let consoleText = document.getElementById('console-log');
 
 function makeatoms(num) {
   var atoms += num;
+  document.getElementById("atomcounter").innerHTML = "Atoms: " + atoms;
+}
+
+function consoleStart() {
+  consoleText.innerHTML += "[(Ks#7L-///CONSOLE ERROR --- INSERT QUANTUM PROCESSOR///-!n6$moi&]"
 }
